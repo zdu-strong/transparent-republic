3b2e47c7dc3b619387d0554a4136ba6024fae902
John Williams <zdu.strong@gmail.com>
2026-04-07 15:58:14 +0900

    feat: Add deserializer of BigDecimal

--- code/react/src/common/Server/bigdecimal_json_config.tsx
+++ code/react/src/common/Server/bigdecimal_json_config.tsx
@@ -2,6 +2,5 @@ import { Big, BigDecimal } from 'bigdecimal.js';
 import { TypedJSON } from 'typedjson';
 
 TypedJSON.mapType(BigDecimal, {
-    serializer: (value) => value instanceof BigDecimal ? value?.stripTrailingZeros().toPlainString() : null,
     deserializer: (value) => value === null || value === undefined || typeof value === "string" && value === "" ? null : Big(value),
 })
