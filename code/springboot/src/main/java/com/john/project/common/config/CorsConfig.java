0c6e23991fdc3c0eda060e645638f80ba69a4966
John Williams <zdu.strong@gmail.com>
2026-05-17 15:47:46 +0900

    Revert "feat: modify cors config of springboot project"
    This reverts commit 7b4198917b45084e6440ad3b85c56fbdb6af530e.

--- code/springboot/src/main/java/com/john/project/common/config/CorsConfig.java
+++ code/springboot/src/main/java/com/john/project/common/config/CorsConfig.java
@@ -13,9 +13,7 @@ public class CorsConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins(CorsConfiguration.ALL)
                 .allowedMethods(CorsConfiguration.ALL)
-                .allowedHeaders(CorsConfiguration.ALL)
-                .allowCredentials(true)
-                .allowPrivateNetwork(true);
+                .allowedHeaders(CorsConfiguration.ALL);
     }
 
 }
