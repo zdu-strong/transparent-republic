e5b58ee7ad47c9aa02d6b3be612af935fde1ecc5
John Williams <zdu.strong@gmail.com>
2026-05-26 15:08:07 +0900

    feat: updat versions of dependencies of react project

--- code/react/src/common/WebWorker/RSAUtils/generateKeyPairOfRSA.worker.tsx
+++ code/react/src/common/WebWorker/RSAUtils/generateKeyPairOfRSA.worker.tsx
@@ -5,7 +5,7 @@ registerWebworker(async () => {
     const rsa = new NodeRSA({ b: 4096 });
     rsa.setOptions({ encryptionScheme: "pkcs1" });
     return {
-        privateKey: rsa.exportKey("pkcs8-private-der").toString("hex"),
-        publicKey: rsa.exportKey("pkcs8-public-der").toString("hex"),
+        privateKey: Buffer.from(rsa.exportKey("pkcs8-private-der")).toString("hex"),
+        publicKey: Buffer.from(rsa.exportKey("pkcs8-public-der")).toString("hex"),
     }
 });
