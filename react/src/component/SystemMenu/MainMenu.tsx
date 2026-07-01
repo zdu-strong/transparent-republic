9617d7849470a2703a70a9d5bff850ef2726218d
John Williams <zdu.strong@gmail.com>
2026-07-01 16:01:54 +0900

    feat: modify menu of react project

--- react/src/component/SystemMenu/MainMenu.tsx
+++ react/src/component/SystemMenu/MainMenu.tsx
@@ -6,6 +6,30 @@ import UserInfoMenu from '@component/SystemMenu/UserInfoMenu';
 import { useReactRouterAppProviderNavigation } from '@component/SystemMenu/js/useReactRouterAppProviderNavigation';
 import { isMobilePhone } from "@/common/is-mobile-phone";
 import { GlobalMenuOpen, setGlobalMenuOpen } from '@/common/Server';
+import { style } from 'typestyle';
+
+const dashboardLayoutContainer = style({
+    $nest: {
+        "& > div.MuiBox-root": {
+            width: "100%",
+            height: "100%",
+            flexDirection: "row",
+            position: "static",
+        },
+        "& > div.MuiBox-root > header.MuiPaper-root": {
+            position: "fixed",
+            right: isMobilePhone ? "0px" : "15px",
+            width: isMobilePhone ? "100vw" : "calc(100vw - 15px)",
+        },
+        "& > div.MuiBox-root > header.MuiPaper-root > div.MuiToolbar-root": {
+            marginRight: "0px",
+        },
+        "& > div.MuiBox-root > div.MuiDrawer-root > div.MuiPaper-root": {
+            height: "calc(100vh - 15px)"
+        },
+    },
+    $debugName: "dashboardLayoutContainer"
+});
 
 type Props = {
     children: ReactNode;
@@ -19,24 +43,22 @@ export default observer((props: Props) => {
         navigation={navigation}
         branding={{ title: "", logo: "" }}
     >
-        <DashboardLayout
-            slots={{ toolbarActions: UserInfoMenu }}
-            slotProps={isMobilePhone ? undefined : {
-                header: {
-                    menuOpen: GlobalMenuOpen.menuOpen,
-                    onToggleMenu: setGlobalMenuOpen,
-                    hideMenuButton: false,
-                }
-            }}
-            hideNavigation={isMobilePhone ? undefined : !GlobalMenuOpen.menuOpen}
-        >
-            <div className="overflow-auto flex flex-auto">
-                <div className="flex flex-col min-w-full min-h-full w-max">
-                    <div className='flex flex-col flex-auto'>
-                        {props.children}
-                    </div>
+        <div className={`flex flex-col flex-auto ${dashboardLayoutContainer}`}>
+            <DashboardLayout
+                slots={{ toolbarActions: UserInfoMenu }}
+                slotProps={isMobilePhone ? undefined : {
+                    header: {
+                        menuOpen: GlobalMenuOpen.menuOpen,
+                        onToggleMenu: setGlobalMenuOpen,
+                        hideMenuButton: false,
+                    }
+                }}
+                hideNavigation={isMobilePhone ? undefined : !GlobalMenuOpen.menuOpen}
+            >
+                <div className="flex flex-col flex-auto">
+                    {props.children}
                 </div>
-            </div>
-        </DashboardLayout>
+            </DashboardLayout>
+        </div>
     </ReactRouterAppProvider>
 })
