8f7a762a7e3784418984e6fe823c1e227cd8e563
John Williams <zdu.strong@gmail.com>
2026-07-27 17:50:05 +0900

    feat: modify LumenExchangeStabilityTest

--- springboot/src/test/java/com/john/project/test/common/lumen/LumenExchangeStabilityTest.java
+++ springboot/src/test/java/com/john/project/test/common/lumen/LumenExchangeStabilityTest.java
@@ -3,13 +3,13 @@ package com.john.project.test.common.lumen;
 import cn.hutool.core.util.NumberUtil;
 import com.john.project.model.LumenContextCoreModel;
 import com.john.project.test.common.BaseTest.BaseTest;
+import io.reactivex.rxjava3.core.Flowable;
 import org.jinq.orm.stream.JinqStream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -19,37 +19,32 @@ public class LumenExchangeStabilityTest extends BaseTest {
 
     @Test
     public void test() {
-        var result = this.lumenContext.exchange(lumenContext.getUsd(), new BigDecimal(100));
-        var resultTwo = this.lumenContext.exchange(lumenContext.getUsd(), new BigDecimal(100));
-        var resultThree = this.lumenContext.exchange(lumenContext.getUsd(), new BigDecimal(100));
-        var resultFour = this.lumenContext.exchange(lumenContext.getUsd(), new BigDecimal(100));
-        var resultFive = this.lumenContext.exchange(lumenContext.getUsd(), new BigDecimal(100));
-        var resultSix = this.lumenContext.exchange(lumenContext.getUsd(), new BigDecimal(100));
-        var resultSeven = this.lumenContext.exchange(lumenContext.getUsd(), new BigDecimal(100));
-        var resultEight = this.lumenContext.exchange(lumenContext.getUsd(), new BigDecimal(100));
-        var resultNine = this.lumenContext.exchange(lumenContext.getUsd(), new BigDecimal(100));
+        var listOfExchangeToUsd = Flowable.range(1, 100)
+                .map(s -> this.lumenContext.exchange(lumenContext.getJapan(), new BigDecimal(50)))
+                .toList()
+                .blockingGet();
+        var listOfExchangeToJapan = Flowable.range(1, 100)
+                .map(s -> this.lumenContext.exchange(lumenContext.getUsd(), new BigDecimal(100)))
+                .toList()
+                .blockingGet();
         var resultTen = this.lumenContext.exchange(lumenContext.getUsd(), new BigDecimal("2000000000000000000000000000000"));
-        var resultOfMaxRatio = resultTen.divide(new BigDecimal("100000000000000000"), 6, RoundingMode.FLOOR);
-        var countOfStabilityExchange = JinqStream.from(List.of(
-                result,
-                resultTwo,
-                resultThree,
-                resultFour,
-                resultFive,
-                resultSix,
-                resultSeven,
-                resultEight,
-                resultNine
-        )).where(s -> NumberUtil.equals(new BigDecimal("50"), s.setScale(5, RoundingMode.HALF_UP))).count();
-        assertEquals(new BigDecimal("66666666666661722.222224"), resultTen);
+        var resultOfMaxRatio = resultTen.divide(new BigDecimal("1000000000000"), 6, RoundingMode.FLOOR);
+        var countOfStabilityExchangeToUsd = JinqStream.from(listOfExchangeToUsd)
+                .where(s -> NumberUtil.equals(new BigDecimal("100"), s.setScale(2, RoundingMode.HALF_UP)))
+                .count();
+        var countOfStabilityExchangeToJapan = JinqStream.from(listOfExchangeToJapan)
+                .where(s -> NumberUtil.equals(new BigDecimal("50"), s.setScale(2, RoundingMode.HALF_UP)))
+                .count();
+        assertEquals(new BigDecimal("666666666666.666611"), resultTen);
         assertEquals(new BigDecimal("0.666666"), resultOfMaxRatio);
-        assertEquals(9, countOfStabilityExchange);
+        assertEquals(listOfExchangeToUsd.size(), countOfStabilityExchangeToUsd);
+        assertEquals(listOfExchangeToJapan.size(), countOfStabilityExchangeToJapan);
     }
 
     @BeforeEach
     public void beforeEach() {
         this.lumenContext = new LumenContextCoreModel();
-        this.lumenContext.injectPair(new BigDecimal("200000000000000000"), new BigDecimal("100000000000000000"));
+        this.lumenContext.injectPair(new BigDecimal("2000000000000"), new BigDecimal("1000000000000"));
     }
 
 }
