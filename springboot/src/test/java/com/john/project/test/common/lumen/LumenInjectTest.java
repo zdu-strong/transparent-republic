3723471bae9dc0c26b711c3216e174d2ea3843fe
John Williams <zdu.strong@gmail.com>
2026-07-29 06:31:03 +0900

    feat: add LumenInjectTest

---
+++ springboot/src/test/java/com/john/project/test/common/lumen/LumenInjectTest.java
@@ -0,0 +1,29 @@
+package com.john.project.test.common.lumen;
+
+import cn.hutool.core.util.ObjectUtil;
+import com.john.project.model.LumenContextCoreModel;
+import com.john.project.test.common.BaseTest.BaseTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class LumenInjectTest extends BaseTest {
+
+    private LumenContextCoreModel lumenContext;
+
+    @Test
+    public void test() {
+        var result = this.lumenContext.inject(lumenContext.getUsd(), new BigDecimal(100));
+        assertTrue(ObjectUtil.equals(new BigDecimal("33.333333"), result));
+    }
+
+    @BeforeEach
+    public void beforeEach() {
+        this.lumenContext = new LumenContextCoreModel();
+        this.lumenContext.injectPair(new BigDecimal(100), new BigDecimal(100));
+    }
+
+}
