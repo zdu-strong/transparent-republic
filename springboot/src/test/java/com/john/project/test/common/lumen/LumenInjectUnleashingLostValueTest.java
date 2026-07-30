474488843d30e0f12960aa02ba2dafa395123cf3
John Williams <zdu.strong@gmail.com>
2026-07-31 05:18:01 +0900

    feat: add unit tests

---
+++ springboot/src/test/java/com/john/project/test/common/lumen/LumenInjectUnleashingLostValueTest.java
@@ -0,0 +1,38 @@
+package com.john.project.test.common.lumen;
+
+import cn.hutool.core.util.ObjectUtil;
+import com.john.project.model.LumenContextCoreModel;
+import com.john.project.test.common.BaseTest.BaseTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class LumenInjectUnleashingLostValueTest extends BaseTest {
+
+    private LumenContextCoreModel lumenContext;
+
+    @Test
+    public void test() {
+        var obtainJapanCurrencyBalance = this.lumenContext.exchange(lumenContext.getUsd(), new BigDecimal(1));
+        var result = this.lumenContext.injectPair(new BigDecimal("198"), new BigDecimal(100).add(obtainJapanCurrencyBalance));
+        var usdCurrencyBalance = this.lumenContext.getUsdCurrencyBalance();
+        var japanCurrencyBalance = this.lumenContext.getJapanCurrencyBalance();
+        var totalCcuBalance = this.lumenContext.getTotalCcuBalance();
+        assertTrue(ObjectUtil.equals(new BigDecimal("50.000000"), obtainJapanCurrencyBalance));
+        assertTrue(ObjectUtil.equals(new BigDecimal("249.000000"), result));
+        assertTrue(ObjectUtil.equals(new BigDecimal("200"), usdCurrencyBalance));
+        assertTrue(ObjectUtil.equals(new BigDecimal("200"), japanCurrencyBalance));
+        assertTrue(ObjectUtil.equals(new BigDecimal("251.000000"), totalCcuBalance));
+    }
+
+    @BeforeEach
+    public void beforeEach() {
+        this.lumenContext = new LumenContextCoreModel();
+        this.lumenContext.injectPair(new BigDecimal(1), new BigDecimal(100));
+    }
+
+}
+
