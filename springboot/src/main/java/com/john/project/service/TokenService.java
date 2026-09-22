13a8aa5b7fc88d58c0fc50f222e2a62ef7fca805
John Williams <zdu.strong@gmail.com>
2026-09-22 15:46:37 +0900

    feat: modify getDecodedJWTOfAccessToken method of TokenService

--- springboot/src/main/java/com/john/project/service/TokenService.java
+++ springboot/src/main/java/com/john/project/service/TokenService.java
@@ -72,8 +72,9 @@ public class TokenService extends BaseService {
         var userId = decodedJWT.getSubject();
         var exists = this.streamAll(TokenEntity.class)
                 .where(s -> s.getId().equals(id))
-                .where(s -> s.getUser().getId().equals(userId))
                 .where(s -> !s.getIsDeleted())
+                .where(s -> s.getUser().getId().equals(userId))
+                .where(s -> !s.getUser().getIsDeleted())
                 .exists();
         if (!exists) {
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Please login first and then visit");
